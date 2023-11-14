2498082931272b141e756f2401e2b224add13ba2
henryjamison <jamisonhc@appstate.edu>
2023-11-14 00:07:59 -0500

    .

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        ppr: true,
+      },
+}
 
 module.exports = nextConfig
 const withVideos = require('next-videos')
